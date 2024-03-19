fce3a25f96054118247b10628063eaaa2e2e87d9
detroitnatif <tytydizzle124@gmail.com>
2024-03-19 11:51:40 +0100

    Fixing api call

---
+++ next-openai/pages/api/hello.ts
@@ -0,0 +1,31 @@
+import type {
+  NextApiRequest,
+  NextApiResponse
+} from 'next'
+
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
+
+
+type Data = {
+  name: string
+}
+
+export default async function handler (
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+          const chatCompletion = await openai.chat.completions.create({
+          messages: [{ role: 'user', content: 'Say this is a test' }],
+          model: 'gpt-3.5-turbo',
+          });
+  
+          const responseText = chatCompletion.choices?.[0]?.message?.content || 'Default response';
+
+          // Now send back a response with the structure that matches `Data`
+          res.status(200).json({ name: responseText });
+      }
+          ;
